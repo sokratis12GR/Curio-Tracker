645c473a211d0f6a0fa9eec78ae604cd8e4f0fd4
Sokratis Fotkatzikis <sokratis12GR@users.noreply.github.com>
2025-07-30 20:52:48 +0300

    Changed the layout of config.py
    Added some more entries such as user, league, area level, layout and support for future changes and for the project that this tool was originally made for

--- config.py
+++ config.py
@@ -1,6 +1,78 @@
 # Enable DEBUGGING
 DEBUGGING = False
 
+# Char Info (for logging)
+poe_league = "3.26"
+poe_user = "sokratis12GR"
+default_bp_lvl = "83"
+default_bp_area = "Prohibited Library"
+
+layout_keywords = [
+    "Bunker", "Records Office", "Mansion", "Smuggler's Den", "Underbelly",
+    "Laboratory", "Prohibited Library", "Repository", "Tunnels"
+]
+TOP_RIGHT_CUT_WIDTH = 366
+TOP_RIGHT_CUT_HEIGHT = 210
+
+# Tesseract Executatble Location (README.MD)
+tesseract_path = r"D:\Dev\Tesseract-OCR\tesseract.exe"
+
+# Keys / Strings / Locations / Texts
+
+## Logging
+logs_dir = "logs"
+saves_dir = "saved"
+csv_file_path = "saved/matches.csv"
+
+### CSV Header Format -- TODO: Adjust to support the format of the "PoE Curio Case Rates" Project: https://docs.google.com/spreadsheets/d/1dDDMRc3GAE4G0X-lJeLXSHaGtLLNf612nrFiOyCo0Vs/edit?gid=710775455#gid=710775455
+csv_league_header = "League"
+csv_loggedby_header = "Logged By"
+csv_blueprint_header = "Blueprint Type"
+csv_area_level_header = "Area Level"
+
+csv_trinket_header = "Trinket"
+csv_replacement_header = "Replacement"
+csv_replica_header = "Replica"
+csv_experimented_header = "Experimented Base Type"
+csv_weapon_enchant_header = "Weapon Enchantment"
+csv_armor_enchant_trinket_header = "Armor Enchantment"
+csv_scarab_trinket_header = "Scarab"
+csv_currency_trinket_header = "Currency"
+csv_stack_size_trinket_header = "Stack Size"
+csv_variant_trinket_header = "Variant"
+csv_flag_trinket_header = "Flag?"
+
+csv_time_header = "Time"
+csv_type_header = "Type"
+csv_value_header = "Value"
+
+### Input Value fo "Valid Experimental/Replicas/Scarabs/Currencies/Enchants/Replacements"
+file_name = "AllValid.csv"
+target_application = "Path of Exile"
+not_found_target_txt = "❌ Path of Exile window not found."
+
+
+## Matches Info
+matches_found = "✅ Matches found:"
+matches_not_found = "❌ No matches found."
+
+
+## Capturing / Exiting
+info_show_keys_capture = "🖼️ Press F2 to capture All Curios on Screen w/o duplicates."
+info_show_keys_snippet = "✂️ Press F4 to snippet a region to capture w/ duplicates."
+info_show_keys_layout = "🗺️ Press F5 to set current layout (type, alvl) data."
+info_show_keys_exit = "❌ Press F3 to exit the script.\n"
+
+capture_key = "f2"
+exit_key = "f3"
+snippet_key = "f4"
+layout_capture_key = "f5"
+
+capturing_prompt = "📸 Capturing screen..."
+layout_prompt = "📸 Capturing layout..."
+exiting_prompt = "👋 Exiting."
+
+
 # HSV thresholds (Hue, Saturation, Value) split to lower and upper values
 
 ## Replica / Unique: #AF6025
@@ -52,35 +124,3 @@ enchant_l_val = 70
 enchant_u_hue = 130
 enchant_u_sat = 90
 enchant_u_val = 240
-
-
-# Tesseract Executatble Location (README.MD)
-tesseract_path = r"D:\Dev\Tesseract-OCR\tesseract.exe"
-
-# Keys / Strings / Locations / Texts
-
-## Logging
-logs_dir = "logs"
-saves_dir = "saved"
-csv_file_path = "saved/matches.csv"
-csv_time_header = "Time"
-csv_type_header = "Type"
-csv_value_header = "Value"
-
-file_name = "AllValid.csv"
-target_application = "Path of Exile"
-not_found_target_txt = "❌ Path of Exile window not found."
-
-
-## Matches Info
-matches_found = "✅ Matches found:"
-matches_not_found = "❌ No matches found."
-
-
-## Capturing / Exiting
-info_show_keys_1 = "🖼️ Press F2 to capture Path of Exile window."
-info_show_keys_2 = "❌ Press F3 to exit the script.\n"
-capture_key = "f2"
-exit_key = "f3"
-capturing_prompt = "📸 Capturing screen..."
-exiting_prompt = "👋 Exiting."
